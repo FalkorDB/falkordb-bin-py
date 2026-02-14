d76c3d5e9a15ac70b76571c153261cf0398469b2
Guy Korland <gkorland@gmail.com>
2026-02-14 15:01:44 +0200

    Normalize macOS arm64 wheel tag to macosx_11_0

--- setup.py
+++ setup.py
@@ -9,6 +9,11 @@ class bdist_wheel(_bdist_wheel):
         super().finalize_options()
         self.root_is_pure = False
 
+    def get_tag(self):
+        python_tag, abi_tag, platform_tag = super().get_tag()
+        if platform_tag.endswith("macosx_15_0_arm64") or platform_tag.endswith("macosx_14_0_arm64"):
+            platform_tag = "macosx_11_0_arm64"
+        return python_tag, abi_tag, platform_tag
+
 
 setup(cmdclass={"bdist_wheel": bdist_wheel})
-
